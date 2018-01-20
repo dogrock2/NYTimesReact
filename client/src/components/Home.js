684118ea189bc600471ed86df057bf7ac0d2d681
Alex <dogrock2@Alexanders-MacBook-Pro.local>
2018-01-20 12:26:42 -0500

    chng socket2

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -18,7 +18,7 @@ class Home extends React.Component {
             open: false
         };
 
-        this.socket = openSocket('127.0.0.1:3001');
+        this.socket = openSocket();
 
         this.socket.on('articleAddedMsg', data => {
                 this.chngModal("Articles", data.message);
