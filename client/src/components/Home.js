3bd1ad5823afb4e7880d823dea2e00187856054a
Alex <dogrock2@Alexanders-MacBook-Pro.local>
2018-01-20 14:11:23 -0500

    aa

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -18,7 +18,7 @@ class Home extends React.Component {
             open: false
         };
 
-        this.socket = openSocket('http://localhost');
+        this.socket = openSocket();
 
         this.socket.on('articleAddedMsg', data => {
                 this.chngModal("Articles", data.message);
